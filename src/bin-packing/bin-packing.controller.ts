8fa5f4bf3ac110ff2530ad1c0b1ecaa13c8c2696
André Figueiredo <afigueiredo@tasken.com>
2023-04-20 14:49:01 -0300

    refactor: remove commented lines from solve method

--- src/bin-packing/bin-packing.controller.ts
+++ src/bin-packing/bin-packing.controller.ts
@@ -13,13 +13,11 @@ export class BinPackingController {
       new Product(1, 5, 10, 5, 15),
       new Product(2, 7, 20, 10, 10),
       new Product(3, 3, 15, 5, 10),
-      // Adicione mais produtos conforme necessário
     ];
 
     const packages: Package[] = [
       new Package(1, 20, 25, 15, 30),
       new Package(2, 15, 25, 10, 25),
-      // Adicione mais embalagens conforme necessário
     ];
 
     const packageProductMap = this.binPackingService.firstFitDecreasing3D(
